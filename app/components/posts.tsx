4536ba2178847a5a946863b2022853e6f3ea2e0c
Yujin Lee <yujinyll@naver.com>
2025-01-28 22:38:31 +0900

    post: 블로그 글 추가

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -1,6 +1,5 @@
-import Link from "next/link";
-import { baseUrl } from "app/sitemap";
-import { formatDate, getBlogPosts } from "app/blog/utils";
+import { getBlogPosts } from "app/blog/utils";
+import Post from "./post";
 
 export function BlogPosts() {
   const allBlogs = getBlogPosts();
@@ -13,44 +12,7 @@ export function BlogPosts() {
             ? -1
             : 1
         )
-        .map((post) => (
-          <Link
-            key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
-            href={`/blog/${post.slug}`}
-          >
-            <li>
-              <img
-                className="rounded-xl w-full aspect-3/2 object-cover mb-3"
-                src={
-                  post.metadata.image
-                    ? `${baseUrl}/images/${post.metadata.image}`
-                    : `/og?title=${encodeURIComponent(post.metadata.title)}`
-                }
-                alt={`"${post.metadata.title}"의 포스트 이미지`}
-              />
-
-              {post.metadata.tags && (
-                <ul className="flex flex-wrap gap-1 mb-3">
-                  {post.metadata.tags?.map((tag) => (
-                    <li className="rounded-md bg-slate-100/20 px-1.5 py-0.5 border border-slate-200 font-medium text-slate-500 text-xs tabular-nums">
-                      {tag}
-                    </li>
-                  ))}
-                </ul>
-              )}
-              <h2 className="font-semibold text-lg text-slate-950 mb-2 leading-6 line-clamp-1 md:line-clamp-2 tracking-tight">
-                {post.metadata.title}
-              </h2>
-              <p className="font-normal text-sm text-slate-700 line-clamp-2 mb-2 tracking-tight">
-                {post.metadata.summary}
-              </p>
-              <p className="font-normal text-slate-500 text-xs tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
-              </p>
-            </li>
-          </Link>
-        ))}
+        .map((post) => Post(post))}
     </ul>
   );
 }
